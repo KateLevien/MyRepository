67b2c5b2423d8031454f98eda47f87880e8d3425
KateLevien <31364400+KateLevien@users.noreply.github.com>
2020-06-15 18:40:27 +0900

    Cloned a Git Repository.

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -3,6 +3,7 @@ public class MyHello {
 	public void sayhello() {
 		System.out.println("Hello!");
 		System.out.println("Sayhello changed in master branch!");
+		System.out.println("Wow!");
 	}
 
 }
